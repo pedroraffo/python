da2c87aaae61b8571484a0ea1e208a9600ed7829
pedroraffo <165916629+pedroraffo@users.noreply.github.com>
2024-05-27 11:26:37 -0300

    Add files via upload

--- guia7.py
+++ guia7.py
@@ -1,4 +1,5 @@
 import array 
+import math 
 
 # eje 1 
 def pertenece (seq : list , n : int) -> bool: 
@@ -45,4 +46,78 @@ def palabras7 (l : list [str]) -> bool:
     return res
      
    
+## 
+
+#1.8
+def cuenta_bancaria (movimientos : list) -> float: 
+   res : float = 0 
+   for tuplas in movimientos: 
+      if tuplas[0] == "I": 
+         res += tuplas[1] 
+      else: 
+         if tuplas[0] == "R": 
+            res -= tuplas [1]
+   return res 
+      
+#print (cuenta_bancaria ([("I", 2000), ("R", 20), ("R", 1000), ("I", 300)])) 
+ 
+#1.9 
+
+def esVocal(letra:str) -> bool:
+    vocales = ["a","e","i","o","u","A","E","I","O","U"]
+    if pertenece(vocales, letra):
+        return True
+    return False
+
+def vocales(palabra:str) -> bool:
+    vocales: int = 0
+    
+    for letra in palabra: 
+        if esVocal(letra):
+            vocales+=1
+    
+    if vocales >= 3:
+        return True
+    return False
+
+#print(vocales("Alamo")) 
+
+#2.1 
+
+def tuvieja (list : int) ->  None : 
+   for i in range(0,len(list),1): 
+      if (i % 2 == 0): 
+         list[i] = 0 
+      
+listaaux : list [int] =  ([1, 2, 3, 4])   
+#print (listaaux)
+#tuvieja (listaaux) 
+#print  (listaaux)
+
+# 2.3
+
+def sinvocales (list) -> list: 
+   nueva : list = [] 
+   for letra in list: 
+      if (esVocal (letra) == False): 
+         nueva.append (letra)
+        
+   return nueva 
    
+#print (sinvocales (["h", "o", "l", "a"]))   
+
+
+### 
+
+# 5.2 
+def pertenece_each_one2 (seq : list, e : int ) -> list [bool]: 
+   nueva : list = [] 
+   for secuencias in seq: 
+      if pertenece (secuencias, e) == True: 
+         nueva.append (True) 
+      else: 
+         nueva.append (False) 
+   return nueva 
+
+print (pertenece_each_one2 ([[1, 7, 2], [4, 5, 2]], 1))       
+      
